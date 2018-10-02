4f3a2d0f41db5ae6241199915d9a3168900b4985
Kenny Tan <kennytanzxc1@gmail.com>
2018-10-01 23:44:09 -0400

    viewcontroller and photocell

--- iOS_tumblr_feed/PhotosViewController.swift
+++ iOS_tumblr_feed/PhotosViewController.swift
@@ -8,14 +8,56 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet weak var tumblrPhotoTableView: UITableView!
+    
+    var posts: [[String: Any]] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-    }
+        self.tumblrPhototableView.delegate = self
+        self.tumblrPhototableView.dataSource = self
 
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                print (self.posts)
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let posts = self.posts {
+            return posts.count
+        }
+        return 0
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        return Photocell?
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
